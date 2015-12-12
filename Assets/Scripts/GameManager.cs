c801df55e39e2f9af78711f893069c6a5e53d780
J.Javier Delgado <dcjosej@gmail.com>
2015-12-12 16:17:21 +0100

    Script GameManager y Gorwbar

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	
+	private static GameManager _instance;
+	public static GameManager instance
+	{
+		get
+		{
+			if (_instance == null)
+			{
+				_instance = GameObject.FindObjectOfType<GameManager>();
+			}
+			return _instance;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void InstantiatePiece()
+	{
+		throw new System.NotImplementedException();
+	}
+}
---
+++ Assets/Scripts/GrowBar.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GrowBar : MonoBehaviour {
+
+	private Slider growBar;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Increase(int amount)
+	{
+		growBar.value += amount;
+
+		if(growBar.value >= 1)
+		{
+			GameManager.instance.InstantiatePiece();
+		}
+    }
+
+}
