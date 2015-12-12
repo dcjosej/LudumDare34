015006d8d606150b2a8c6d35708d26427a8d6012
Manuel J. González <majegonro@gmail.com>
2015-12-12 13:22:11 +0100

    Project Settings y anchors test

---
+++ Assets/Scripts/AnchorViewer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnchorViewer : MonoBehaviour {
+
+    Anchor[] anchors;
+
+    void OnEnable()
+    {
+        anchors = transform.GetComponentsInChildren<Anchor>();
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (anchors.Length > 0)
+        {
+            for (int i = 0; i < anchors.Length; i++)
+            {
+                Gizmos.color = Color.yellow;
+                Gizmos.DrawSphere(anchors[i].transform.position, 0.2f);
+            }
+        }
+
+    }
+
+
+}
